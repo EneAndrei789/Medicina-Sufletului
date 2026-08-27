7dc5dcac1f549e3209f760ca81d6fb0f0d6cc6aa
EneAndrei789 <andreileonardene@gmail.com>
2026-08-27 14:09:13 +0200

    energii nefavorabile

---
+++ Medicina-Sufletului/src/App/Home/modules/dezechilibru/Corpul Fizic/Energie/energii-nefavorabile/energii-nefavorabile.jsx
@@ -0,0 +1,223 @@
+import React from "react";
+import "./energii-nefavorabile.css";
+
+export default function TabelulEnergiilorNefavorabile() {
+
+    const righe = [
+        {
+            nome: 'Randul 1',
+            colonnaA: [
+                'Victima',
+                'Coplesire',
+                'Nerabdare',
+                'Dezgust',
+                'Dezinterest',
+                'Dezordine',
+                'Lene',
+                'Bantuire',
+                'Lipsa de iubire'
+            ],
+
+            colonnaB: [
+                'Neicredere',
+                'Nervozitate',
+                'Ignorare',
+                'Agresivitate',
+                'Linguseala',
+                'Confuzie',
+                'Santaj',
+                'Energie de incapacitate',
+                'Jena'
+            ]
+        },
+
+        {
+            nome: 'Randul 2',
+            colonnaA: [
+                'Gelozie',
+                'Sadism',
+                'Masochism',
+                'Nehotarare',
+                'Carcoteala',
+                'Melancolie',
+                'Dispretuire',
+                'Mandrie',
+                'Energie diabolica'
+            ],
+
+            colonnaB: [
+                'Nemultumire',
+                'Rigiditate',
+                'Irascibilitate',
+                'Atasament',
+                'Perfidie',
+                'Narcisism',
+                'Acuzare',
+                'Frica de suferinte',
+                'Fuga de raspundere'
+            ]
+        },
+
+        {
+            nome: 'Randul 3',
+            colonnaA: [
+                'Ingrijorare',
+                'Sabotaj',
+                'Critica',
+                'Rusine',
+                'Ipocrizie',
+                'Dominatie',
+                'Inversunare',
+                'Egoism',
+                'Nepasare'
+            ],
+
+            colonnaB: [
+                'Egomanie',
+                'Instabilitate psihica',
+                'Dorinta vs lipsa',
+                'Viclenie',
+                'Masturbare',
+                'Claustofobie',
+                'Desfranare',
+                'Mizerie',
+                'Malnutritie sufleteasca'
+            ]
+        },
+
+        {
+            nome: 'Randul 4',
+            colonnaA: [
+                'Incalcire',
+                'Vampirism',
+                'Distorsionare',
+                'Sufocare',
+                'Emotivitate',
+                'Umilinta',
+                'Stagnare',
+                'Energie Otravitoare',
+                'Dilatare'
+            ],
+
+            colonnaB: [
+                'Razbunare',
+                'Imprastiere',
+                'Incrancenare',
+                'Nesiguranta',
+                'Epuizare',
+                'Deznadejde',
+                'Durere sufleteasca',
+                'Toxicitate',
+                'Nostalgie'
+            ]
+        },
+
+        {
+            nome: 'Randul 5',
+            colonnaA: [
+                'Timp comprimat',
+                'Lacomie',
+                'Indiferenta',
+                'Inhibare',
+                'Furie',
+                'Neputinta',
+                'Inferioritate',
+                'Insesibilitate',
+                'Teroare'
+            ],
+
+            colonnaB: [
+                'Tristete',
+                'Izolare',
+                'Incapatanare',
+                'Plumb',
+                'Mercur',
+                'Mitomanie',
+                'Indecizie',
+                'Intovertire',
+                'Lipsa de curaj'
+            ]
+        },
+
+        {
+            nome: 'Randul 6',
+            colonnaA: [
+                'Timiditate',
+                'Neacceptare',
+                'Frica',
+                'Suparare',
+                'Amaraciune',
+                'Manie',
+                'Deshidratare sufleteasca',
+                'Respingere',
+                'Deficit de bucurie'
+            ],
+
+            colonnaB: [
+                'Fobie obsesiva',
+                'Barfa',
+                'Obsesie',
+                'Paranoia',
+                'Ostilitate',
+                'Frustrare',
+                'Strangulare',
+                'Grandomanie',
+                'Violenta'
+            ]
+         },
+
+         {
+            nome: 'Randul 7',
+            colonnaA: [
+                'Energie de gheata',
+                'Slabiciune',
+                'Stres',
+                'Creier spalat',
+                'Manipulare',
+                'Imbufnare',
+                'Intimidare',
+                'Neglijenta',
+                'Incapacitate de comunicare'
+            ],
+
+            colonnaB: [
+                'Megatomanie',
+                'Apatie',
+                'Intunecare',
+                'Avaritie',
+                'Rautate',
+                'Energie distructiva',
+                'Infidelitate',
+                'Mahnire'
+            ]
+         },
+
+         {
+            nome: 'Randul 8',
+            colonnaA: [
+                'Abuz de sine',
+                'Datorie',
+                'Furt',
+                'Energie care te innebuneste',
+                'Instabilitate vegetativa',
+                'Greutate',
+                'Impulsivitate',
+                'Energie de calcifiere',
+                'Regret'
+            ],
+
+            colonnaB: [
+                'Abandon',
+                'Rivalitate',
+                'Invidie',
+                'Aroganta',
+                'Limitare',
+                'Enervare',
+                'Dominare',
+                'Inflexibilitate',
+                'Scepticism'
+            ]
+         }
+    ]
+
+}
